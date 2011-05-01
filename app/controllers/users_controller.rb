0550a17cc95712ab20d7a7247592db27175e59f0
Mike Mennis <mmennis@gmail.com>
2011-05-01 00:26:29 -0700

    User signup functionality complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Do whatever for new user ...
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
