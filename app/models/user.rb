dbb018ab430ed0fd2037b378791d6ad69777079b
Mike Mennis <mmennis@gmail.com>
2011-04-30 23:22:34 -0700

    change some small stuff in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,25 +39,25 @@ class User < ActiveRecord::Base
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    user && user.has_password?(submitted_password) ? user : nil
   end
   
   private
   
-  def encrypt_password
-    self.salt = make_salt if new_record?
-    self.encrypted_password = encrypt(password)
-  end
-  
-  def encrypt(string)
-    secure_hash("#{salt}--#{string}")
-  end
-  
-  def make_salt
-    secure_hash("#{Time.now.utc}--#{password}")
-  end
-  
-  def secure_hash(string)
-    Digest::SHA2.hexdigest(string)
-  end
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
